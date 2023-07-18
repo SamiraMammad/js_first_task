b4a1aea0ca85120582c64b8079d6953c78e05a96
Samira <samire.memmedova750@gmail.com>
2023-07-18 07:13:04 +0400

    first

---
+++ JS_firsttask.js
@@ -0,0 +1,103 @@
+// task1
+
+// const firstname=prompt("Please enter your first name");
+// console.log(firstname);
+
+
+
+
+
+
+
+
+// task2
+
+// let firstNumber = parseInt(prompt("Please enter your first number"));
+// let secondNumber = parseInt(prompt("Please enter your second number"));
+
+// function numbers(a,b)
+// {
+// if(a===1 && b===2)
+// return a+b;
+// else {
+//     console.log("Entered number is uncorrect");
+// }
+// }
+// console.log (numbers(firstNumber,secondNumber));
+
+
+
+
+
+
+
+
+
+// task3
+
+// let x=parseInt(prompt("Please enter greater than 100"));
+
+// console.log (`Your number is ${x}`);
+
+// while (x<100){
+//     let x=parseInt(prompt("Please enter greater than 100 again"));
+//     console.log (`Your number is ${x}`);    
+
+//     if(x>100){
+//         break;
+//     }
+        
+// } 
+// if(x>100){
+// console.log (`You entered ${x}`);
+// }
+
+
+
+
+
+
+
+// task4
+
+// let x=parseInt(prompt("Enter number"));
+// if(x>0){
+//     alert (`${x} greater than zero`);
+// }
+// else if(x<0){
+//     alert ( `${x} less than zero` );
+// }
+// else{
+//     alert (`${x} equals zero`);
+// }
+
+
+
+
+
+
+
+
+
+// task 5
+// const message = prompt("Enter your Message ");
+// message = 'employee' ? message = 'director' ? message == null ? 
+// alert("hello") : alert("greetings") : alert("No login") : alert("Nothing");
+
+
+
+
+
+
+
+// task 6
+// let number=parseInt(prompt("enter number"));
+// let number2=parseInt(prompt("enter pow of number"));
+// function numbers (a,b)
+// {
+// return a**b;
+
+// }
+// console.log(numbers(number,number2));
+
+
